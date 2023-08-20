be58a769026f57fdde10e4167ba3d04f729dcf73
Chenyang <moluuser@gmail.com>
2023-08-20 09:45:10 +0800

    Fix moving direction

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,31 +14,30 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
-
-        float zAngle = transform.rotation.eulerAngles.z;
-
-        float h = Input.GetAxisRaw("Horizontal"); // -1, 0, 1
-        transform.Translate(Vector3.right * h * Time.deltaTime * speed, Space.World);
-        if (h < 0)
-        {
-            transform.rotation = Quaternion.Euler(0, 0, zAngle + 90);
-        }
-        else if (h > 0)
-        {
-            transform.rotation = Quaternion.Euler(0, 0, zAngle - 90);
-        }
-
         float v = Input.GetAxisRaw("Vertical"); // -1, 0, 1
         transform.Translate(Vector3.up * v * Time.deltaTime * speed, Space.World);
         if (v < 0)
         {
-
-            transform.rotation = Quaternion.Euler(0, 0, zAngle + 180);
+            transform.rotation = Quaternion.Euler(0, 0, 180);
         }
         else if (v > 0)
         {
             transform.rotation = Quaternion.Euler(0, 0, 0);
         }
+        if (v != 0)
+        {
+            return;
+        }
 
+        float h = Input.GetAxisRaw("Horizontal"); // -1, 0, 1
+        transform.Translate(Vector3.right * h * Time.deltaTime * speed, Space.World);
+        if (h < 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 90);
+        }
+        else if (h > 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 90);
+        }
     }
 }
